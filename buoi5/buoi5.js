165368093241dbf2684020e4f87c0a564b401334
Hoang Anh <zhindo100899@gmail.com>
2023-09-24 18:17:17 +0700

    them file baitap buoi 5

---
+++ buoi5/buoi5.js
@@ -0,0 +1,31 @@
+// bai 1
+
+// let hocVien1 = {
+//     name : "Nguyễn Hoàng Anh",
+//     age : 26,
+//     job : "hoc vien",
+
+//     inThongtin : function() {
+//     console.log("tôi là học viên lớp app k16");
+//     }
+//         }
+
+// console.log(hocVien1);
+// hocVien1.inThongtin();
+
+//bai 2
+
+// function inTen() {
+//     console.log("Toi ten la Nguyen Van A");
+// }
+ // nó sẽ không ra kết quả vì phương thức này k có đối tượng.
+
+ // bai 3 
+
+ function daysUntilFutureDate(futureDate){
+    
+ }
+
+ let targetDate = new Date("2023-09-27");
+ console.log("số ngày còn lại cho đến ngày") + targetDate.toDateString() + " là"
++ daysUntilFutureDate(targetDate) + " ngày.";
